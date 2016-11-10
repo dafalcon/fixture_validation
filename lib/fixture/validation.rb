cfcecd177ceebc53b71a40c14fdc80bd45496ac7
Dan Falcone <dafalcon@akamai.com>
2016-11-09 23:01:46 -0500

    Initial commit

---
+++ lib/fixture/validation.rb
@@ -0,0 +1,7 @@
+require "fixture/validation/version"
+
+module Fixture
+  module Validation
+    # Your code goes here...
+  end
+end
---
+++ lib/fixture/validation/version.rb
@@ -0,0 +1,5 @@
+module Fixture
+  module Validation
+    VERSION = "0.1.0"
+  end
+end
