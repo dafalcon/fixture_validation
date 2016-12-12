035ed5601b5c183f982e8adb3e58e8ada94a403c
Dan Falcone <dafalcon@akamai.com>
2016-12-12 18:02:38 -0500

    Use fixtures from the dummy app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,11 +9,9 @@ require 'rails/test_help'
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
-
-# Load fixtures from the engine
+# Load fixtures from the dummy app
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
+  ActiveSupport::TestCase.fixture_path = File.expand_path('../dummy/test/fixtures', __FILE__)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + '/files'
-  ActiveSupport::TestCase.fixtures :all
 end
